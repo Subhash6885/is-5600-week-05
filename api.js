728cb927769f44a890860d5ca51596d704d157f4
Subhash6885 <subhashchandra.karuturi@slu.edu>
2025-04-25 00:44:57 -0500

    Update api.js

--- api.js
+++ api.js
@@ -2,6 +2,7 @@ const path = require('path')
 const Products = require('./products')
 const autoCatch = require('./lib/auto-catch')
 
+
 /**
  * Handle the root route
  * @param {object} req
@@ -49,30 +50,52 @@ async function getProduct(req, res, next) {
  * @param {object} req 
  * @param {object} res 
  */
-async function createProduct(req, res) {
-  console.log('request body:', req.body)
-  res.json(req.body)
+async function createProduct(req, res, next) {
+  const product = await Products.create(req.body)
+  res.json(product)
 }
 
-/**
- * Edit a product
- * @param {object} req
- * @param {object} res
- * @param {function} next
- */
-async function editProduct(req, res, next) {
-  console.log(req.body)
-  res.json(req.body)
-}
 
-/**
- * Delete a product
- * @param {*} req 
- * @param {*} res 
- * @param {*} next 
- */
-async function deleteProduct(req, res, next) {
-  res.json({ success: true })
+async function editProduct (req, res, next) {
+  const change = req.body
+  const product = await Products.edit(req.param.id, change)
+  res.json(product)
+}
+async function deleteProduct (req, res, next) {
+  const response = await Products.destroy(req.params.id)
+  res.json(response)
+}
+async function createOrder (req, res, next) {
+  const order = await Orders.create(req.body)
+  res.json(orders)
+}
+async function listOrders (req, res, next) {
+  const { offset = 0, limit = 25, productId, status } = req.query
+
+  const orders = await Orders.list({ 
+    offset: Number(offset), 
+    limit: Number(limit),
+    productId, 
+    status 
+  })
+
+  res.json(orders)
+}
+async function editOrder(req, res, next) {
+  try {
+      const updatedOrder = await orders.edit(req.params.id, req.body);
+      res.json(updatedOrder);
+  } catch (err) {
+      next(err);
+  }
+}
+async function deleteOrder(req, res, next) {
+  try {
+      await orders.destroy(req.params.id);
+      res.status(204).send(); // No content
+  } catch (err) {
+      next(err);
+  }
 }
 
 module.exports = autoCatch({
@@ -81,5 +104,9 @@ module.exports = autoCatch({
   getProduct,
   createProduct,
   editProduct,
-  deleteProduct
+  deleteProduct,
+  listOrders,
+  createOrder,
+  editOrder,
+  deleteOrder,
 });
